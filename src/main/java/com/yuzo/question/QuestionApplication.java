99646b4cc11fe9c7a1507bef421740dc61a2e45c
yuanchun <yyuunn9876@163.com>
2019-08-19 16:32:08 +0800

    work table

--- src/main/java/com/yuzo/question/QuestionApplication.java
+++ src/main/java/com/yuzo/question/QuestionApplication.java
@@ -1,14 +1,11 @@
 package com.yuzo.question;
 
-import java.util.List;
+
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.yuzo.question.entity.Question;
 
 @SpringBootApplication
 @MapperScan(value="com.yuzo.question.mapper")
